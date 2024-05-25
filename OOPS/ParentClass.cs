cff4e379fa1fed322622acce456a16a24ee86326
anuplab <anuplab.ck@gmail.com>
2024-05-25 16:27:31 +0530

    OOPS concepts

---
+++ OOPS/ParentClass.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotnetPlayground
+{
+    public class ParentClass
+    {
+        protected int MyProperty { get; } = 50;
+
+        //Overriding
+        public virtual void Caller()
+        {
+            Console.WriteLine("Invoked from Caller method of parent class");
+        }
+
+        public void EmptyCaller() 
+        {
+            Console.WriteLine("Empty Caller from Parent");
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+namespace DotnetPlayground 
+{
+    public class Program
+    {
+
+        public static void Main()
+        {
+            ChildOne childOne = new();
+
+            Console.WriteLine("Child Property value: " + childOne.BaseName);
+            childOne.CallMethod();
+            childOne.CallMethod("Henry");
+
+            ParentClass polymorphicObj = new ChildOne();
+            //Polymorph
+            polymorphicObj.Caller();
+
+            //Not a polymorph
+            polymorphicObj.EmptyCaller();
+            
+        }
+    }
+
+}
